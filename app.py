5e95d6b691a1a9276d7f24483cdbd6c4cf1981ce
UJJWAL10R <urajpal10@gmail.com>
2026-05-06 23:54:13 +0530

    Initial commit

--- app.py
+++ app.py
@@ -67,17 +67,8 @@ DEFAULT_USERS = [
         "locations": "ALL",
         "is_active": 1,
     },
-    {
-        "name": "Mohita Chandiramani",
-        "email": "Mohita.Chandiramani@aequs.com",
-        "password": "15374",
-        "role": "Manager",
-        "permission_level": "Admin",
-        "locations": "ALL",
-        "is_active": 1,
-    },
 ]
- 
+
 DEFAULT_TV_RECORDS = [
     {
         "tv_id": "Ano Treatment",
@@ -97,17 +88,17 @@ DEFAULT_TV_RECORDS = [
     {
         "tv_id": "ACP3-ANOTV",
         "location": "ANO (Floor Side)",
-        "content_name": "ANO_DAILY_UPDATES (KPOV-LB, KPIV-S1, ANO106, ANO-Temp)",
+        "content_name": "ANO_DAILY_UPDATES",
         "template_type": "PICKCELL",
         "layout_composition": "Full Screen",
-        "deployed_date": "29-Apr-2026",
-        "deployed_time": "11:10 AM",
-        "tv_status": "ONLINE",
-        "last_online": "Today",
+        "deployed_date": "23-Apr-2026",
+        "deployed_time": "11:40 AM",
+        "tv_status": "OFFLINE",
+        "last_online": "1 Day Ago",
         "ping_status": "PING REQUEST",
-        "last_ping_time": "10:36 AM",
+        "last_ping_time": "10:33 AM",
         "deployed_status": "Deployed",
-        "remarks": "Healthy screen and current content synced.",
+        "remarks": "Power cycle recommended.",
     },
     {
         "tv_id": "Flash QDIM",
@@ -118,7 +109,7 @@ DEFAULT_TV_RECORDS = [
         "deployed_date": "23-Apr-2026",
         "deployed_time": "04:15 PM",
         "tv_status": "OFFLINE",
-        "last_online": "5 Days Ago",
+        "last_online": "Today",
         "ping_status": "PING REQUEST",
         "last_ping_time": "09:58 AM",
         "deployed_status": "Deployed",
@@ -133,11 +124,11 @@ DEFAULT_TV_RECORDS = [
         "deployed_date": "03-Apr-2026",
         "deployed_time": "10:00 AM",
         "tv_status": "OFFLINE",
-        "last_online": "26 Days Ago",
+        "last_online": "20 Days Ago",
         "ping_status": "PING REQUEST",
         "last_ping_time": "10:34 AM",
         "deployed_status": "Deployed",
-        "remarks": "Awaiting line-sides hardware inspection.",
+        "remarks": "Awaiting line-side hardware inspection.",
     },
 ]
 
